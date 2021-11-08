f384ff47a4250b278f12a4f7926f4f64d6e5ce6f
evan <44725772+Evanavevan@users.noreply.github.com>
2021-11-08 16:46:46 +0800

    Add files via upload

---
+++ spring-cloud-alibaba-my-shop/myshop-service-provider-item/src/main/java/com/evan/myshop/service/provider/item/controller/TbItemController.java
@@ -0,0 +1,7 @@
+package com.evan.myshop.service.provider.item.controller;
+
+import com.evan.myshop.commons.domain.TbContent;
+import com.evan.myshop.commons.web.AbstractBaseController;
+
+public class TbItemController extends AbstractBaseController<> {
+}
